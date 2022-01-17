afdbb46ee972f0668cc3f4df5ffbf5f649f99006
GoyaChen <46504384+GoyaChen@users.noreply.github.com>
2022-01-16 18:53:00 -0600

    complete

--- Main/Stack/headers/Stack.h
+++ Main/Stack/headers/Stack.h
@@ -16,6 +16,21 @@ public:
 	/*****************************************/
 	/** WHATEVER CODE YOU NEED TO ADD HERE!! */
 	/*****************************************/
+    Data getHoldMe() const {
+        return holdMe;
+    }
+
+    void setHoldMe(Data holdMeData) {
+        Node::holdMe = holdMeData;
+    }
+
+    Node *getNext() const {
+        return next;
+    }
+
+    void setNext(Node *nextData) {
+        Node::next = nextData;
+    }
 
 };
 
@@ -28,20 +43,42 @@ class Stack {
 public:
 
 	// destroys the stack
-	~Stack () { /* your code here */ }
+	~Stack () { /* your code here */
+        while(head != NULL){
+            Node<Data>* oldHead = head;
+            head = head->getNext();
+            delete oldHead;
+        }
+    }
 
 	// creates an empty stack
-	Stack () { /* your code here */ }
+	Stack () {/* your code here */
+        head = NULL;
+    }
 
 	// adds pushMe to the top of the stack
-	void push (Data) { /* your code here */ }
+	void push (Data pushMe) { /* your code here */
+        auto* newHead = new Node<Data>;
+        newHead->setHoldMe(pushMe);
+        newHead->setNext(head);
+        head = newHead;
+    }
 
 	// return true if there are not any items in the stack
-	bool isEmpty () { /* replace with your code */ return true; }
+	bool isEmpty () { /* replace with your code */
+        if (head == NULL) return true;
+        else return false;
+    }
 
 	// pops the item on the top of the stack off, returning it...
 	// if the stack is empty, the behavior is undefined
-	Data pop () { /* replace with your code */ return Data ();  }
+	Data pop () { /* replace with your code */
+        Data result = head->getHoldMe();
+        Node<Data>* oldHead = head;
+        head = head->getNext();
+        delete oldHead;
+        return result;
+    }
 };
 
 #endif
